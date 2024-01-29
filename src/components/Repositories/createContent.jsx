2762a6669a09632ec4fa04efacd27078b52c1d78
MaratRizatdinov <maratrizatdinov91@gmail.com>
2024-01-29 15:19:24 +0300

    Readme

--- src/components/Repositories/createContent.jsx
+++ src/components/Repositories/createContent.jsx
@@ -1,37 +1,34 @@
-import * as S from './repositories.style'
+import * as S from "./repositories.style";
 
-export const createContent = (repos) => {
-    const tableContent = [];
-    const columns = ["name", "description", "link"];
-    for (let elem of repos) {
-      for (let column of columns) {
-        if (column === "name") {
-          tableContent.push(
-            <S.ReposTableCell key={elem.name}>{elem.name}</S.ReposTableCell>
-          );
-        } else if (column === "description") {
-          tableContent.push(
-            <S.ReposTableCell key={elem.description}>
-              {elem.description}
-            </S.ReposTableCell>
-          );
-        } else {
-          tableContent.push(
-            <S.ReposTableCell
-              key={elem.clone_url}
-              style={{ justifyContent: "center" }}
-            >
-              <a
-                href={elem.clone_url}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                перейти
-              </a>
-            </S.ReposTableCell>
-          );
-        }
+export const createContent = (repos) => {  
+  console.log(repos)
+  const tableContent = [];
+  const columns = ["name", "description", "link"];
+  for (let elem of repos) {
+    for (let column of columns) {
+      if (column === "name") {
+        tableContent.push(
+          <S.ReposTableCell key={elem.id+elem.name}>{elem.name}</S.ReposTableCell>
+        );
+      } else if (column === "description") {
+        tableContent.push(
+          <S.ReposTableCell key={elem.id+elem.description}>
+            {elem.description}
+          </S.ReposTableCell>
+        );
+      } else {
+        tableContent.push(
+          <S.ReposTableCell
+            key={elem.id+elem.clone_url}
+            style={{ justifyContent: "center" }}
+          >
+            <a href={elem.clone_url} target="_blank" rel="noopener noreferrer">
+              перейти
+            </a>
+          </S.ReposTableCell>
+        );
       }
     }
-    return tableContent;
-  };
+  }
+  return tableContent;
+};
